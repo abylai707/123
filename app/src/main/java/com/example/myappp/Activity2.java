b84ff5aecb19c0df9fde50d51178bb215cc8e13c
Abylai <abylai707@gmail.com>
2020-02-16 00:13:39 +0600

    firest

---
+++ app/src/main/java/com/example/myappp/Activity2.java
@@ -0,0 +1,33 @@
+package com.example.myappp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Activity2 extends AppCompatActivity {
+    private Button button1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+
+
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+    }
+    public void openActivity2() {
+        Intent intent = new Intent(this, Activity3.class);
+        startActivity(intent);
+    }
+    }
+
+
